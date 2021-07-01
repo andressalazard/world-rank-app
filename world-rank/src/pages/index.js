8aa937f17441d0a2669247a8acd8b6104a8ab455
andressalazard <andres.salazar.d.18@gmail.com>
2021-07-01 18:29:27 -0500

    created CountriesTable component
    created and styled the component
    
    display every country within countries list
    
    added function to sort countries list by population

--- world-rank/src/pages/index.js
+++ world-rank/src/pages/index.js
@@ -3,13 +3,16 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout/Layout';
 import SearchInput from '../components/SearchInput/SearchInput';
+import CountriesTable from '../components/CountriesTable/CountriesTable';
 
 export default function Home({ countries }) {
-  console.log(countries);
   return <Layout>
     <div className={styles.counts}>Found {countries.length} countries</div>
     <SearchInput placeholder="Filter by Name, Region or Subregion" />
+    <CountriesTable countries={countries} />
   </Layout>
+
+
 }
 
 export const getStaticProps = async () => {
