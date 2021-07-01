6da0c15d34fecc476e8a19969a705d6a5fde4786
andressalazard <andres.salazar.d.18@gmail.com>
2021-07-01 16:53:58 -0500

    created component Layout

---
+++ world-rank/src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout/Layout';
+
+export default function Home({ countries }) {
+  console.log(countries);
+  return <Layout>main</Layout>
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    }
+  }
+}
